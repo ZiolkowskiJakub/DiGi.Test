3a49bb7853c7cbcd1c41b418e23dbe0d7ecdde2f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-02-18 22:46:43 +0100

    Initial Implementation

---
+++ DiGi.Test/Classes/TestObject.cs
@@ -0,0 +1,80 @@
+﻿using DiGi.Core.Classes;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace DiGi.Core.Test.Classes
+{
+    public class TestObject : UniqueObject
+    {
+        [JsonInclude, JsonPropertyName("Name")]
+        private string name;
+
+        [JsonInclude, JsonPropertyName("UniqueReference")]
+        private UniqueReference uniqueReference;
+
+        [JsonInclude, JsonPropertyName("Range")]
+        private Range<double> range;
+
+        [JsonInclude, JsonPropertyName("DateTime")]
+        private DateTime dateTime = DateTime.MinValue;
+
+        [JsonInclude, JsonPropertyName("Color")]
+        private System.Drawing.Color color = System.Drawing.Color.Empty;
+
+        [JsonInclude, JsonPropertyName("IndexedDoubles")]
+        private IndexedObjects<double> indexedDoubles = null;
+
+        public TestObject(string name)
+            : base()
+        {
+            this.name = name;
+            uniqueReference = new UniqueReference(this);
+        }
+
+
+        public TestObject(string name, double min, double max)
+            : base()
+        {
+            this.name = name;
+            uniqueReference = new UniqueReference(this);
+            range = new Range<double>(min, max);
+            dateTime = DateTime.Now;
+            color = System.Drawing.Color.Blue;
+
+            indexedDoubles = new IndexedObjects<double>();
+            indexedDoubles.Add(0, 10);
+            indexedDoubles.Add(1, 20);
+        }
+
+        public TestObject(Guid guid, string name)
+            : base(guid)
+        {
+            this.name = name;
+            uniqueReference = new UniqueReference(this);
+        }
+
+        public TestObject(JsonObject jsonObject)
+            :base(jsonObject)
+        {
+
+        }
+
+        [JsonIgnore]
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        [JsonIgnore]
+        public UniqueReference UniqueReference
+        {
+            get
+            {
+                return uniqueReference;
+            }
+        }
+    }
+}
---
+++ DiGi.Test/Windows/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Nodes;
+using System.Windows;
+using DiGi.Core.Test.Classes;
+
+namespace DiGi.Core.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Test1_Click(object sender, RoutedEventArgs e)
+        {
+            TestObject testObject_5 = new TestObject("CC", 10, 12);
+            TestObject testObject_6 = testObject_5.Clone<TestObject>();
+            string json = Convert.ToString(testObject_5, new System.Text.Json.JsonSerializerOptions() { WriteIndented = true });
+
+
+            //TestObject testObject = new TestObject("AAA");
+
+            //JsonValue? jsonValue = JsonValue.Create(testObject);
+
+            //JsonObject jsonObject = Convert.ToJson(testObject);
+
+            //TestObject testObject_3 = Create.SerializableObject<TestObject>(jsonObject);
+
+            TestObject testObject_2 = new TestObject("BBB");
+            json = Convert.ToString(testObject_2);
+
+            JsonObject jsonObject = JsonObject.Parse(json).AsObject();
+
+            TestObject testObject_3 = Create.SerializableObject<TestObject>(jsonObject);
+
+            TestObject testObject_4 = testObject_3.Clone<TestObject>();
+
+            //string a = "";
+
+            //int hashCode = a.GetHashCode();
+
+            //Modify.FromJsonObject(testObject_2, jsonObject);
+        }
+    }
+}
