5c12b9d4beba1aa875bba4008ef7fc69d938c29d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-02-21 22:35:58 +0100

    Tests Reorganized

---
+++ Geometry/Windows/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using DiGi.Geometry.Planar.Classes;
+
+namespace DiGi.Geometry.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Test1_Click(object sender, RoutedEventArgs e)
+        {
+            Vector2D vector2D = new Vector2D(10, 11);
+
+            string json = DiGi.Core.Convert.ToString(new List<Vector2D> { vector2D, vector2D });
+
+            List<Vector2D> vector2Ds = DiGi.Core.Convert.ToDiGi<Vector2D>(json);
+
+            //JsonNode jsonNode = JsonNode.Parse(json);
+
+
+            //ISerializableObject serializableObject = Create.SerializableObject<ISerializableObject>(jsonNode.AsObject());
+        }
+    }
+}
