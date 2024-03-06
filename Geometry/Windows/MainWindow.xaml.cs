1d4c9d6332378c22c8a449fc5f301f995fc17429
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-03-06 22:07:10 +0100

    Test Updated

--- Geometry/Windows/MainWindow.xaml.cs
+++ Geometry/Windows/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.Json.Nodes;
 using System.Windows;
+using DiGi.Core;
+using DiGi.Core.Interfaces;
 using DiGi.Geometry.Planar.Classes;
 
 namespace DiGi.Geometry.Test
@@ -16,42 +18,89 @@ namespace DiGi.Geometry.Test
 
         private void Button_Test1_Click(object sender, RoutedEventArgs e)
         {
-            IntersectionResult2D intersectionResult2D = Geometry.Planar.Create.IntersectionResult2D(new Line2D(new Point2D(0, 0), new Vector2D(0, 1)), new Line2D(new Point2D(1, 0), new Vector2D(0, 1)));
+            Rectangle2D rectangle2D = new Rectangle2D(new Point2D(1, 1), 20, 10);
 
-            Vector2D vector2D_1 = new Vector2D(0, 1);
+            Polyline2D polyline2D = new Polyline2D(new List<Point2D>() 
+            { 
+                new Point2D(0, 0) ,
+                new Point2D(10, 0),
+                new Point2D(10, 10),
+                new Point2D(5, 10),
+                 new Point2D(5, -10),
+            });
 
-            Vector2D vector2D_2 = new Vector2D(0, -6);
-
-
-            List<KeyValuePair<double, double>> values = new List<KeyValuePair<double, double>>();
-            values.Add(new KeyValuePair<double, double>(1, 2));
-            values.Add(new KeyValuePair<double, double>(3, 5));
-
-            JsonValue jsonValue_Temp = JsonValue.Create(values);
-
-            List<KeyValuePair<double, double>> values_Temp = jsonValue_Temp.GetValue<List<KeyValuePair<double, double>>>();
+            List<Polygon2D> polygon2Ds = DiGi.Geometry.Planar.Create.Polygon2Ds(polyline2D);
 
 
+            Vector2D widthDirection = rectangle2D.WidthDirection;
+            Vector2D heightDirection = rectangle2D.HeightDirection;
 
-            Point2D point2D = new Point2D(1, 1);
-            Vector2D vector2D_3 = new Vector2D(0, 1);
-            Transform2D transform2D = Transform2D.GetRotation(Math.PI);
+            List<Point2D> point2Ds_1 = rectangle2D.GetPoints();
+            rectangle2D.Inverse();
 
-            point2D.Transform(transform2D);
-            vector2D_3.Transform(transform2D);
+            List<Point2D> point2Ds_2 = rectangle2D.GetPoints();
+
+            double s = 0;
+
+            //GeometryCollection2D geometryCollection2D = new GeometryCollection2D();
+            //geometryCollection2D.Add(new Segment2D(0,0, 0, 1));
+            //geometryCollection2D.Add(new Point2D(0, 0));
+
+            //GeometryCollection2D geometryCollection2D_Temp = new GeometryCollection2D();
+            //geometryCollection2D_Temp.Add(new Segment2D(0, 0, 1, 3));
+            //geometryCollection2D.Add(geometryCollection2D_Temp);
+
+            //geometryCollection2D.Transform(Transform2D.GetTranslation(1, 1));
+
+
+            //string json = DiGi.Core.Convert.ToString((ISerializableObject)geometryCollection2D);
+
+            //GeometryCollection2D? geometryCollection2D_Converted = DiGi.Core.Convert.ToDiGi<GeometryCollection2D>(json)?.FirstOrDefault();
+
+
+
+            //double s = 1;
+
+            //IntersectionResult2D intersectionResult2D = Geometry.Planar.Create.IntersectionResult2D(new Line2D(new Point2D(0, 0), new Vector2D(0, 1)), new Line2D(new Point2D(1, 0), new Vector2D(0, 1)));
+
+            //Vector2D vector2D_1 = new Vector2D(0, 1);
+
+            //Vector2D vector2D_2 = new Vector2D(0, -6);
+
+
+            //List<KeyValuePair<double, double>> values = new List<KeyValuePair<double, double>>();
+            //values.Add(new KeyValuePair<double, double>(1, 2));
+            //values.Add(new KeyValuePair<double, double>(3, 5));
+
+            //JsonValue jsonValue_Temp = JsonValue.Create(values);
+
+            //List<KeyValuePair<double, double>> values_Temp = jsonValue_Temp.GetValue<List<KeyValuePair<double, double>>>();
+
+
+
+            //Point2D point2D = new Point2D(1, 1);
+            //Vector2D vector2D_3 = new Vector2D(0, 1);
+            //Transform2D transform2D = Transform2D.GetRotation(Math.PI);
+            ////transform2D = Transform2D.GetTranslation(1, 1);
+
+            //Segment2D segment2D = new Segment2D(new Point2D(0, 0), new Point2D(0, 1));
+            //segment2D.Transform(transform2D);
+
+            //point2D.Transform(transform2D);
+            //vector2D_3.Transform(transform2D);
+
+            //CoordinateSystem2D coordinateSystem2D = new CoordinateSystem2D();
 
 
 
 
+            //List<int[]> indexes = new List<int[]>() { new int[] {1, 2, 3 }, new int[] { 2, 2, 3 } };
 
+            //JsonValue jsonValue = JsonValue.Create(indexes);
 
-            List<int[]> indexes = new List<int[]>() { new int[] {1, 2, 3 }, new int[] { 2, 2, 3 } };
+            //List<int[]> indexes_Temp = jsonValue.GetValue<List<int[]>>();
 
-            JsonValue jsonValue = JsonValue.Create(indexes);
-
-            List<int[]> indexes_Temp = jsonValue.GetValue<List<int[]>>();
-
-            bool collinear = (new Line2D(new Point2D(0, 0), vector2D_1)).Collinear(new Line2D(new Point2D(2, 2), vector2D_2));
+            //bool collinear = (new Line2D(new Point2D(0, 0), vector2D_1)).Collinear(new Line2D(new Point2D(2, 2), vector2D_2));
 
 
             //Vector2D vector2D = new Vector2D(10, 11);
