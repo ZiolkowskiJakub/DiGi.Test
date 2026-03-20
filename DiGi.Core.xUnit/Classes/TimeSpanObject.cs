514b9d9db97308bae20ec0db659dda1d9896c6de
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-03-20 07:41:28 +0100

    TimeSpan conversion test added

---
+++ DiGi.Core.xUnit/Classes/TimeSpanObject.cs
@@ -0,0 +1,38 @@
+using DiGi.Core.Classes;
+using DiGi.Core.Interfaces;
+using DiGi.Core.xUnit.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace DiGi.Core.xUnit
+{
+    public class TimeSpanObject : SerializableObject, ISerializableObject
+    {
+        [JsonInclude, JsonPropertyName("TimeSpan")]
+        public TimeSpan TimeSpan { get; set; }
+
+        public TimeSpanObject()
+        {
+
+        }
+
+        public TimeSpanObject(TimeSpanObject timeSpanObject)
+            : base(timeSpanObject)
+        {
+            if(timeSpanObject is not null)
+            {
+                TimeSpan = timeSpanObject.TimeSpan;
+            }
+        }
+
+        public TimeSpanObject(JsonObject jsonObject)
+            : base(jsonObject)
+        {
+
+        }
+    }
+}
---
+++ DiGi.Core.xUnit/Tests/TimeSpan.cs
@@ -0,0 +1,22 @@
+﻿using DiGi.Core.Classes;
+using DiGi.Core.IO.Core.Interfaces;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Text.Json;
+
+namespace DiGi.Core.xUnit
+{
+    public partial class Tests
+    {
+        [Fact]
+        public void TimeSpan()
+        {
+            TimeSpanObject timeSpanObject = new () {TimeSpan = new System.TimeSpan(1,2,3,4,5,6)};
+
+            string jsonString = JsonSerializer.Serialize(timeSpanObject);
+
+
+            Query.SerializationCheck(timeSpanObject);
+
+        }
+    }
+}
