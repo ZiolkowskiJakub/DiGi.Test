c904291014874706a0844a80f841ef00ccb4a829
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-05-12 06:36:08 +0200

    Unit xUnit test added

---
+++ DiGi.Unit.xUnit/Tests/TryConvert.cs
@@ -0,0 +1,36 @@
+﻿namespace DiGi.Unit.xUnit
+{
+    public partial class Tests
+    {
+        [Fact]
+        public void TryConvert()
+        {
+            bool converted;
+            double? value;
+
+            converted = Query.TryConvert(10, Enums.LengthUnit.Meter, Enums.LengthUnit.Milimeter, out value);
+            
+            Assert.True(converted);
+
+            Assert.Equal(10000, value);
+
+            converted = Query.TryConvert(2, Enums.LengthUnit.Meter, Enums.LengthUnit.Feet, out value);
+
+            Assert.True(converted);
+
+            Assert.Equal(6.561679790026246, value);
+
+            converted = Query.TryConvert(2, Enums.TemperatureUnit.Kelvin, Enums.TemperatureUnit.Fahrenheit, out value);
+
+            Assert.True(converted);
+
+            Assert.Equal(-456.07, value);
+
+            converted = Query.TryConvert(2, Enums.TimeUnit.Second, Enums.TimeUnit.Minute, out value, Core.Constants.Tolerance.MacroDistance);
+
+            Assert.True(converted);
+
+            Assert.Equal(0.033, value);
+        }
+    }
+}
